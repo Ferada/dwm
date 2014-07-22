26c0d7615956c91abcf6268e7c54e40530bd176f
Olof-Joachim Frahm <olof@macrolet.net>
2014-07-22 20:33:02 +0100

    Change default theme, keybindings.

--- config.def.h
+++ config.def.h
@@ -1,34 +1,28 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <XF86keysym.h>
+
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const char *fonts[]          = { "unifont:size=12" };
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { "#bbbbbb", "#111111", "#333333" },
+	[SchemeSel]  = { "#eeeeee", "#222222", "#222222" },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",     NULL,       NULL,       1 << 6,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "feh",      NULL,       NULL,       0,            1,           -1 },
+	{ "MPlayer",  NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -44,7 +38,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -54,10 +48,27 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+void
+exec(const Arg *arg) {
+	if(dpy)
+		close(ConnectionNumber(dpy));
+	setsid();
+	execvp(((char **)arg->v)[0], (char **)arg->v);
+	fprintf(stderr, "dwm: execvp %s", ((char **)arg->v)[0]);
+	perror(" failed");
+	exit(EXIT_SUCCESS);
+}
+
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[]            = {"dmenu_run", "-b", "-m", dmenumon, NULL};
+static const char *termcmd[]             = {"urxvt", NULL};
+static const char *dwmcmd[]              = {"dwm", NULL};
+static const char *raisevolumecmd[]      = {"amixer", "set", "Master", "5%+", NULL};
+static const char *smallraisevolumecmd[] = {"amixer", "set", "Master", "1%+", NULL};
+static const char *lowervolumecmd[]      = {"amixer", "set", "Master", "5%-", NULL};
+static const char *smalllowervolumecmd[] = {"amixer", "set", "Master", "1%-", NULL};
+static const char *mutecmd[]             = {"amixer", "set", "Master", "toggle", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -93,7 +104,13 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{MODKEY|ControlMask|ShiftMask,  XK_q,      exec,           {.v = dwmcmd}},
+	{MODKEY|ShiftMask,              XK_q,      quit,           {0}},
+	{0,               XF86XK_AudioRaiseVolume, spawn,          {.v = raisevolumecmd}},
+	{ShiftMask,       XF86XK_AudioRaiseVolume, spawn,          {.v = smallraisevolumecmd}},
+	{0,               XF86XK_AudioLowerVolume, spawn,          {.v = lowervolumecmd}},
+	{ShiftMask,       XF86XK_AudioLowerVolume, spawn,          {.v = smalllowervolumecmd}},
+	{0,                      XF86XK_AudioMute, spawn,          {.v = mutecmd}},
 };
 
 /* button definitions */
