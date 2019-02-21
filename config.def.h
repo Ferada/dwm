ba09fb180fa78ad5296d196293dea7bc358ae59d
Olof-Joachim Frahm (欧雅福) <olof@macrolet.net>
2019-02-21 21:11:30 +0100

    Add fibonacci patch.

--- config.def.h
+++ config.def.h
@@ -40,12 +40,15 @@ static const int layoutaxis[] = {
 };
 
 #include "horizgrid.c"
+#include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "###",      horizgrid },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
 };
 
 /* key definitions */
@@ -104,6 +107,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[4]} },
+	{ MODKEY|ShiftMask,             XK_g,      setlayout,      {.v = &layouts[5]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
